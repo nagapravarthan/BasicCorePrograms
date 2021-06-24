bdf209f7aacfaaf164e4ec008e5c86638192ecf2
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-25 01:02:12 +0530

    package BasicPrograms;.java

--- src/basiccoreprogram/Basiccore.java
+++ src/basiccoreprogram/Basiccore.java
@@ -2,24 +2,25 @@ package basiccoreprogram;
 
 import java.util.Scanner;
 
-public class Basiccore
+public class Basiccore 
 {
-	public static void main(String[] args) {
+
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-		
-		System.out.println("Enter 1st number: ");
-		Scanner s=new Scanner(System.in);
-		int a=s.nextInt();
-		System.out.println("Enter 2nd number: ");
-		Scanner s1=new Scanner(System.in);
-		int b=s1.nextInt();
-		System.out.println("Before Swapping: "+a+" "+b);
-		int temp;
-		temp=a;
-		a=b;
-		b=temp;
-		System.out.println("After swapping: "+a+" "+b);
+		System.out.println("enter a number");
+	       Scanner s=new Scanner(System.in);
+	       int n= s.nextInt();
+	       if(n%2==0)
+	       {
+	    	   System.out.println("number is even");
+	       }
+		  else
+		  {
+			  System.out.println("number is odd");
 
 	}
-
+	       
+	}
+	
 }
