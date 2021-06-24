57e504f3fff06217a2fdef53614dcf8a8a442ef3
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-25 01:21:53 +0530

    Largestof3Numbers.java

--- src/basiccoreprogram/Basiccore.java
+++ src/basiccoreprogram/Basiccore.java
@@ -2,20 +2,21 @@ package basiccoreprogram;
 
 import java.util.Scanner;
 
-public class Basiccore 
-{
+public class Basiccore  {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter single aplhabet: ");
-		char ch=s.next().charAt(0);
-		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
-			System.out.println(ch+" is a vowel");
+		System.out.println("Enter 3 numbers: ");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		if(a>b&&a>c)
+			System.out.println(a+" is the largest number");
+		else if(b>a&&b>c)
+			System.out.println(b+" is the largest number");
 		else
-			System.out.println(ch+" is a Consonant");
-		
+			System.out.println(c+" is the largest number");
 	}
 
 }
