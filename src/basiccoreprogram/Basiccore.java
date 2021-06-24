b7cad4eb87a98e3513722cc3e07c6bbbba176afd
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-25 01:15:07 +0530

    VowelsAndConsonants.java

--- src/basiccoreprogram/Basiccore.java
+++ src/basiccoreprogram/Basiccore.java
@@ -8,19 +8,14 @@ public class Basiccore
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("enter a number");
-	       Scanner s=new Scanner(System.in);
-	       int n= s.nextInt();
-	       if(n%2==0)
-	       {
-	    	   System.out.println("number is even");
-	       }
-		  else
-		  {
-			  System.out.println("number is odd");
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter single aplhabet: ");
+		char ch=s.next().charAt(0);
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+			System.out.println(ch+" is a vowel");
+		else
+			System.out.println(ch+" is a Consonant");
+		
+	}
 
-	}
-	       
-	}
-	
 }
