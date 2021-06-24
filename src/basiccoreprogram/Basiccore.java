a5062859182ef6d5f41c6999ffbe64766a5a97a3
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-25 00:43:09 +0530

    primefactor.java

--- src/basiccoreprogram/Basiccore.java
+++ src/basiccoreprogram/Basiccore.java
@@ -10,17 +10,24 @@ public class Basiccore
 	{
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter a number: ");
+		System.out.println("Enter a number");
 		int n=s.nextInt();
-		double H=0;
-		if(n>0)
+		while(n%2==0)
 		{
-		for(int i=1;i<=n;i++)
-		H=H+(1.0/i);
-		System.out.println("Harmonic number is: "+H);
+			System.out.println(2+" ");
+			n/=2;
 		}
-		else
-			System.out.println("Enter number greater than 0");		
+		for(int i=3;i<=Math.sqrt(n);i+=2)
+		{
+			while(n%i==0)
+			{
+				System.out.println(i+" ");
+				n/=i;
+			}
+		}
+		if(n>2)
+			System.out.print(n);
+
 	}
 
 }
