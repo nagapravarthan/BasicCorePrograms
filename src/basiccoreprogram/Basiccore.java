5cd8202fd7557d5512e76088f42650a3923850bc
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-25 00:11:51 +0530

    Leapyear

--- src/basiccoreprogram/Basiccore.java
+++ src/basiccoreprogram/Basiccore.java
@@ -1,34 +1,31 @@
 package basiccoreprogram;
 
+import java.util.Scanner;
+
 public class Basiccore
+
+
+
 	{
+
 		public static void main(String[] args) 
 		{
 			// TODO Auto-generated method stub
-			Scanner s = new Scanner(System.in);
-			System.out.println("Enter number of times you want to flip a coin : ");
-			int numberofFlips=s.nextInt();
+			Scanner s=new Scanner(System.in);
+			System.out.println("Enter the year: ");
+			int year =s.nextInt();
 			s.close();
-			if(numberofFlips<=0)
+			if(year<1000)
 			{
-				System.out.println("Please enter positive integer");	
+				System.out.println("Enter a valid Year ");
+			}
+			
+			else if((year%4==0 && year%100!=0) || year%400==0)
+			{
+				System.out.println("Entered year is Leap year");
 			}
 			else
-			{
-				int numberofHeads=0;	
-				for(int i=0;i<numberofFlips;i++)
-				{
-					if(Math.random()>0.5)
-					{
-						numberofHeads++;
-						System.out.println(numberofHeads);
-					}
-				}
-				double percentofHeads=(numberofHeads*100)/numberofFlips;
-				double percentofTails=100-percentofHeads;
-			    System.out.println("Head percentage : "+percentofHeads);
-			    System.out.println("Tails percentage :"+percentofTails);
-			}
+				System.out.println("Entered year is not leap year");
 
 		}
 
